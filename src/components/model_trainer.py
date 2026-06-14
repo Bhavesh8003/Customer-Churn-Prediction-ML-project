a6ed3cf048fa45bb8a010084ec8025d0ddd6e94b
Bhavesh Gulab Badkas <bhaveshbadkas111@gmail.com>
2026-06-15 01:49:45 +0530

    Model Trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,81 @@
+import os 
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostClassifier
+from sklearn.ensemble import (
+    AdaBoostClassifier,
+    GradientBoostingClassifier,
+    RandomForestClassifier
+)
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    confusion_matrix,
+    classification_report
+)
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utlis import save_object,evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path= os.path.join("artifacts","model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+    
+    def inititate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("Splitting training and test input data")
+            X_train,y_train,X_test,y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models = {
+                "Logistic Regression": LogisticRegression(),
+                "Random Forest": RandomForestClassifier(),
+                "Decision Tree": DecisionTreeClassifier(),
+                "Gradient Boosting": GradientBoostingClassifier(),
+                "K-Neighbors Classifier":KNeighborsClassifier(),
+                "XGBClassifier": XGBClassifier(),
+                "CatBoosting Classifier": CatBoostClassifier(verbose=False),
+                "AdaBoost Classifier": AdaBoostClassifier()    
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            ## To get best model score from dict
+            best_model_score= max(sorted(model_report.values()))
+
+            ##To get best model name from dict
+            best_model_name= list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ] 
+            best_model = models[best_model_name]
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best model found on both training and testing dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+            accuracyscore = accuracy_score(y_test, predicted) 
+            return accuracyscore
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+            
